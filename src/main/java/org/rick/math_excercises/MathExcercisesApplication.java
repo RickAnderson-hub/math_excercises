8c7cf7e8ada27a8b5d95603c89fad8ec50385006
Rick <rick@getanderson.net>
2024-07-06 22:32:08 +1200

    init

---
+++ src/main/java/org/rick/math_excercises/MathExcercisesApplication.java
@@ -0,0 +1,13 @@
+package org.rick.math_excercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathExcercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathExcercisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/rick/math_excercises/MathExcercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package org.rick.math_excercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MathExcercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
