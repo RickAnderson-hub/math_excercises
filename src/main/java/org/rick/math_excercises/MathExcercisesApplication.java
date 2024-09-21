c483d1b37000acdd3ed2354a96a00ca660fc4281
Rick <rick@getanderson.net>
2024-09-22 10:38:12 +1200

    removed bean usage

--- src/main/java/org/rick/math_excercises/MathExcercisesApplication.java
+++ src/main/java/org/rick/math_excercises/MathExcercisesApplication.java
@@ -3,6 +3,7 @@ package org.rick.math_excercises;
 import org.rick.math_excercises.model.Equation;
 import org.rick.math_excercises.service.GenerateService;
 import org.rick.math_excercises.service.PdfService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,18 +14,20 @@ import java.util.List;
 @SpringBootApplication
 public class MathExcercisesApplication {
 
+
+    private static GenerateService generateService = new GenerateService();
+
+    private static PdfService pdfService = new PdfService();
+
     public static void main(String[] args) {
         SpringApplication app = new SpringApplication(MathExcercisesApplication.class);
         app.setWebApplicationType(WebApplicationType.NONE);
-        ConfigurableApplicationContext context = app.run(args);
         if (args.length < 2) {
             System.out.println("Usage: java -jar math-excercises.jar <limit> <numberOfExercises>");
             System.exit(1);
         }
-        int limit = Integer.parseInt(args[1]);
-        int numberOfExercises = Integer.parseInt(args[2]);
-        GenerateService generateService = context.getBean(GenerateService.class);
-        PdfService pdfService = context.getBean(PdfService.class);
+        int limit = Integer.parseInt(args[0]);
+        int numberOfExercises = Integer.parseInt(args[1]);
         List<Equation> equations = generateService.generateExercises(limit, numberOfExercises);
         pdfService.generatePdf(equations);
     }
