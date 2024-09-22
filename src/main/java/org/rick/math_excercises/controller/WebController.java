0897758c14319ec9973f0740771487ed9f56b392
Rick <rick@getanderson.net>
2024-09-22 19:19:53 +1200

    added frontend build

--- src/main/java/org/rick/math_excercises/controller/WebController.java
+++ src/main/java/org/rick/math_excercises/controller/WebController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class WebController {
 
-    @RequestMapping(value = "/{[path:[^\\.]*}")
+    @RequestMapping(value = "/{[path:[^.]*}")
     public String redirect() {
         return "forward:/";
     }
