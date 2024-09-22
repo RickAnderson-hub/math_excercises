7ee9410597f1c3856e8101f6f5043e0605feffca
Rick <rick@getanderson.net>
2024-09-22 18:52:25 +1200

    integrated react frontend

---
+++ src/main/java/org/rick/math_excercises/controller/WebController.java
@@ -0,0 +1,13 @@
+package org.rick.math_excercises.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class WebController {
+
+    @RequestMapping(value = "/{[path:[^\\.]*}")
+    public String redirect() {
+        return "forward:/";
+    }
+}
